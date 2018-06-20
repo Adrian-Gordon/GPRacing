c85a9e0bb48f7cf42bb801a07040c3e2a1ea3051
Adrian Gordon <adriangordon60@gmail.com>
2018-06-20 12:21:39 +0100

    node tests

--- GPNode/GPnode.js
+++ GPNode/GPnode.js
@@ -13,12 +13,6 @@ const functionSet = nconf.get('functionSet')
 
 class GPnode {
   constructor ({type, functionname, variablename, value, args}){
-    this.id =  _GPnode_nodeid++
-    this.type = type
-    this.functionname = functionname
-    this.variablename = variablename
-    this.value = value
-
    
     if(typeof type == 'undefined'){
       throw new Error('no node type provided')
@@ -28,11 +22,221 @@ class GPnode {
       throw new Error('invalid node type: ' + type)
     }
 
-    if(typeof this.functionname != 'undefined')
-      this.arity = functionSet[this.functionname].arity
+     //a variable must have a name
 
+    if((type === 'variable')&&(typeof variablename == 'undefined')){
+      throw new Error('a variable must have a variablename')
+    }
+    //it must be a defined variable
+
+    if((type === 'variable')&&!this.inArray(variablename,nconf.get('variables'))){
+      throw new Error("not a valid variable: '" + variablename + "'")
+    }
+   
+
+    //a function must have a name
+
+     if((type === 'function')&&(typeof functionname == 'undefined')){
+      throw new Error('a function must have a functionname')
+    }
+
+     //a function must have a name that is in the valid set of functionnames
+
+    if(type === 'function'){
+      if(typeof functionSet[functionname] == 'undefined')
+      throw new Error("invalid functionname: '" + functionname + "'")
+    }
+
+    //a constant must have a value
+
+     if((type === 'constant')&&(typeof value == 'undefined')){
+      throw new Error('a constant must have a value')
+    }
+
+    if(type === 'function'){
+
+      this.arity = functionSet[functionname].arity
+    }
+
+    this.id =  _GPnode_nodeid++
+    this.type = type
+    this.functionname = functionname
+    this.variablename = variablename
+    this.value = value
+
+    if(type =='function' && typeof args != 'undefined'){
+      this.arguments = args
+    }
+    else if(type == 'function'){
+      this.arguments = new Array(this.arity)
+    }
 
   }
+
+  copy(){
+    let newNode = new GPnode({type:this.type, functionname:this.functionname, variablename:this.variablename, value:this.value})
+
+    if(this.type=='function'){
+       newNode.arguments=new Array(this.arity);
+
+      for(var i=0;i<this.arity;i++){
+        newNode.arguments[i]=this.arguments[i].copy();
+      }
+    }
+    return(newNode)
+  }
+
+  eval(variableBindings){
+    
+    if(this.type == 'constant'){
+      return(this.value)
+    }
+    else if(this.type == 'variable'){
+      let val = variableBindings[this.variablename]
+
+      if(typeof val == 'undefined')
+        throw new Error("variablebindingnotfound: '" + this.variablename + "'")
+      else return(val)
+    }
+  else if(this.type == 'function'){
+
+    //evaluate its arguments
+    let argVals=new Array(this.arity);
+
+    if(this.arity !== this.arguments.length){
+      throw new Error("wrong number of arguments to function: '" + this.functionname + "'")
+    }
+
+    for(var i=0;i<this.arity;i++){
+      argVals[i]=this.arguments[i].eval(variableBindings)
+    }
+    if(this.functionname=='+'){
+      var rval=parseFloat(argVals[0]) + parseFloat(argVals[1])
+      if(isNaN(rval)){
+        
+        return(0.0)
+      }
+      return(rval)
+    }
+    else if(this.functionname=='-'){
+      var rval=parseFloat(argVals[0]) - parseFloat(argVals[1])
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+    else if(this.functionname=='*'){
+      var rval=parseFloat(argVals[0]) * parseFloat(argVals[1])
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+    else if(this.functionname=='/'){
+      var rval=parseFloat(argVals[0]) / parseFloat(argVals[1])
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+    else if(this.functionname=='^'){
+      //logger.info(JSON.stringify(argVals));
+      var rval=Math.pow(argVals[0], Math.floor(argVals[1]))
+      if(!isFinite(rval)){
+        rval=Number.MAX_VALUE
+      }
+      if(isNaN(rval)){
+        //logger.info(JSON.stringify(this) + JSON.stringify(argVals))
+        return(0)
+      }
+      return(rval);
+      
+    }
+    else if(this.functionname=='if<='){
+      if(argVals[0] <= argVals[1]){
+        rval=argVals[2]
+        if(isNaN(rval)){
+          //logger.info(JSON.stringify(this) + JSON.stringify(argVals))
+          return(0)
+        }
+        return(rval)
+      }
+      else{
+        rval=argVals[3]
+        if(isNaN(rval)){
+          //logger.info(JSON.stringify(this) + JSON.stringify(argVals))
+          return(0)
+        }
+        return(rval);
+      }
+    }
+    else if(this.functionname=='cos'){
+      var rval=Math.cos(parseFloat(argVals[0]))
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+    else if(this.functionname=='sin'){
+      var rval=Math.sin(parseFloat(argVals[0]))
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+    else if(this.functionname=='exp'){
+      var rval=Math.exp(parseFloat(argVals[0]))
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+    else if(this.functionname=='log'){
+      let arg = parseFloat(argVals[0])
+      if(arg < 0) arg = -1.0 * arg
+      var rval=Math.log(arg)
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+     else if(this.functionname=='sqrt'){
+      let arg = parseFloat(argVals[0])
+      if(arg < 0) arg = -1.0 * arg
+      var rval=Math.sqrt(arg)
+      if(isNaN(rval)){
+        return(0.0)
+      }
+      return(rval);
+      
+    }
+    
+
+  }
+
+    
+
+  }
+
+inArray(token,array){
+
+  for(var i=0;i<array.length;i++){
+    if(token == array[i])return(true)
+  }
+
+  return(false)
+}
+
+  
+
+
 
 }
 
--- GPNode/GPnode.spec.js
+++ GPNode/GPnode.spec.js
@@ -15,15 +15,282 @@ describe('GPnode', () => {
   it('throws an error if no type is provided', (done) => {
 
     assert.throws(() => new GPnode({}),Error, "no node type provided")
-   // const newnode = new GPnode({})
-    //newnode.type.should.equal('function')
+
     done()
   })
-   it('throws an error if an invalid type is provided', (done) => {
+  it('throws an error if an invalid type is provided', (done) => {
 
-    assert.throws(() => new GPnode({type: 'fizzbang'}),Error, "invalid node type: fizzbang")
-   // const newnode = new GPnode({})
-    //newnode.type.should.equal('function')
+    assert.throws(() => new GPnode({'type': 'fizzbang'}),Error, "invalid node type: fizzbang")
+   
     done()
+  })
+
+  it('throws an error if a variable does not have a variablename', (done) => {
+
+    assert.throws(() => new GPnode({'type': 'variable'}),Error, "a variable must have a variablename")
+   
+    done()
+  })
+
+  it('throws an error if a function does not have a functionname', (done) => {
+
+    assert.throws(() => new GPnode({'type': 'function'}),Error, "a function must have a functionname")
+   
+    done()
+  })
+
+  it('throws an error if a function does not have a functionname that is in the valid set of function names', (done) => {
+
+    assert.throws(() => new GPnode({'type': 'function','functionname':'if=<'}),Error, "invalid functionname: 'if=<'")
+   
+    done()
+  })
+
+  it('throws an error if a constant does not have a value', (done) => {
+
+    assert.throws(() => new GPnode({'type': 'constant'}),Error, "a constant must have a value")
+   
+    done()
+  })
+
+  it('correctly creates the arguments of a function when none are provided', (done) => {
+    let node = new GPnode({'type': 'function','functionname': 'if<='})
+
+    node.arity.should.eql(4)
+    node.arguments.length.should.eql(4)
+
+    done()
+
+  })
+
+  it('correctly creates the arguments of a function when arguments are provided', (done) => {
+    let node = new GPnode({'type': 'function','functionname': 'if<=','args':[1,2,3,4]})
+
+    node.arity.should.eql(4)
+    node.arguments.length.should.eql(4)
+    node.arguments[0].should.eql(1)
+
+    done()
+
+  })
+
+  it('returns a copy of a constant', (done) => {
+    let node = new GPnode({'type':'constant', 'value': 4})
+    let copy = node.copy()
+    copy.type.should.eql('constant')
+    copy.value.should.eql(4)
+
+    done()
+  })
+
+  it('returns a copy of a variable', (done) => {
+    let node = new GPnode({'type':'variable', 'variablename': 'x','value': 4})
+    let copy = node.copy()
+    copy.type.should.eql('variable')
+    copy.variablename.should.eql('x')
+    copy.value.should.eql(4)
+
+
+    done()
+  })
+
+  it('returns a copy of a function, with recursively constructed arguments', (done) => {
+    let node1 = new GPnode({'type':'constant','value': 4})
+    let node2 = new GPnode({'type':'constant','value': 5})
+    let node3 = new GPnode({'type':'function','functionname':'+','args':[node1, node2]})
+
+    let copy = node3.copy()
+
+    copy.type.should.eql('function')
+    copy.functionname.should.eql('+')
+    copy.arguments.length.should.eql(2)
+    copy.arguments[0].type.should.eql('constant')
+    copy.arguments[0].value.should.eql(4)
+    copy.arguments[1].type.should.eql('constant')
+    copy.arguments[1].value.should.eql(5)
+
+    done()
+
+  })
+
+  it('evaluates a constant', (done) => {
+    let node = new GPnode({'type':'constant','value': 4.27})
+
+    let val = node.eval({})
+
+    val.should.eql(4.27)
+
+    done()
+  })
+ /* it('throws an error when evaluating a variable if the variable is not a valid variable ', (done) => {
+    let node = new GPnode({'type':'variable','variablename':'a'})
+
+     assert.throws(() => node.eval({}),Error, "not a valid variable: 'a'")
+
+     done()
+  })*/
+
+
+  it('throws an error when evaluating a variable if the variable binding is not found', (done) => {
+    let node = new GPnode({'type':'variable','variablename':'x'})
+
+     assert.throws(() => node.eval({}),Error, "variablebindingnotfound: 'x'")
+
+     done()
+  })
+
+  it('evaluates a variable', (done) => {
+    let node = new GPnode({'type':'variable','variablename':'x'})
+
+    node.eval({'x':132.7}).should.eql(132.7)
+
+    done()
+  })
+
+  it('throws an error when a function is provided with the wrong number of arguments', (done)=> {
+
+    let node1 = new GPnode({'type':'constant','value': 4.27})
+    let node2 = new GPnode({'type':'function','functionname':'+','args':[node1]})
+    assert.throws(() => node2.eval({}),Error, "wrong number of arguments to function: '+'")
+    done()
+  })
+
+  it('evaluates + function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.27})
+    let node2 = new GPnode({'type':'variable','variablename':'x'})
+    let node3 = new GPnode({'type':'function','functionname':'+','args':[node1, node2]})
+
+    node3.eval({'x':2}).should.eql(6.27)
+
+    done()
+
+  })
+
+  it('evaluates - function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node2 = new GPnode({'type':'variable','variablename':'x'})
+    let node3 = new GPnode({'type':'function','functionname':'-','args':[node1, node2]})
+
+    node3.eval({'x':2.0}).should.eql(2.0)
+
+    done()
+
+  })
+  it('evaluates * function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node2 = new GPnode({'type':'variable','variablename':'x'})
+    let node3 = new GPnode({'type':'function','functionname':'*','args':[node1, node2]})
+
+    node3.eval({'x':2.0}).should.eql(8.0)
+
+    done()
+
+  })
+
+  it('evaluates / function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node2 = new GPnode({'type':'variable','variablename':'x'})
+    let node3 = new GPnode({'type':'function','functionname':'/','args':[node1, node2]})
+
+    node3.eval({'x':2.0}).should.eql(2.0)
+
+    done()
+
+  })
+  it('evaluates ^ function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node2 = new GPnode({'type':'variable','variablename':'x'})
+    let node3 = new GPnode({'type':'function','functionname':'^','args':[node1, node2]})
+
+    node3.eval({'x':2.0}).should.eql(16.0)
+
+    done()
+
+  })
+
+  it('evaluates the if<= function, correctly returning the first argument', (done) => {
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node2 = new GPnode({'type':'constant','value': 5.0})
+    let node3 = new GPnode({'type':'constant','value': 100.0})
+    let node4 = new GPnode({'type':'constant','value': 200.0})
+
+    let node5 = new GPnode({'type':'function','functionname':'if<=','args':[node1, node2, node3, node4]})
+
+    node5.eval({}).should.eql(100.0)
+
+    done()
+
+  })
+
+  it('evaluates the if<= function, correctly returning the second argument', (done) => {
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node2 = new GPnode({'type':'constant','value': 3.0})
+    let node3 = new GPnode({'type':'constant','value': 100.0})
+    let node4 = new GPnode({'type':'constant','value': 200.0})
+
+    let node5 = new GPnode({'type':'function','functionname':'if<=','args':[node1, node2, node3, node4]})
+
+    node5.eval({}).should.eql(200.0)
+
+    done()
+
+  })
+
+  it('evaluates cos function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node3 = new GPnode({'type':'function','functionname':'cos','args':[node1]})
+
+    node3.eval({}).should.eql(Math.cos(4.0))
+
+    done()
+
+  })
+
+  it('evaluates sin function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node3 = new GPnode({'type':'function','functionname':'sin','args':[node1]})
+
+    node3.eval({}).should.eql(Math.sin(4.0))
+
+    done()
+
+  })
+
+  it('evaluates exp function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node3 = new GPnode({'type':'function','functionname':'exp','args':[node1]})
+
+    node3.eval({}).should.eql(Math.exp(4.0))
+
+    done()
+
+  })
+  it('evaluates log function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node3 = new GPnode({'type':'function','functionname':'log','args':[node1]})
+
+    node3.eval({}).should.eql(Math.log(4.0))
+
+    done()
+
+  })
+  it('evaluates log function for negative numbers', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': -4.0})
+    let node3 = new GPnode({'type':'function','functionname':'log','args':[node1]})
+
+    node3.eval({}).should.eql(Math.log(4.0))
+
+    done()
+
+  })
+
+  it('evaluates sqrt function', (done) =>{
+    let node1 = new GPnode({'type':'constant','value': 4.0})
+    let node3 = new GPnode({'type':'function','functionname':'sqrt','args':[node1]})
+
+    node3.eval({}).should.eql(Math.sqrt(4.0))
+
+    done()
+
   })
 })
